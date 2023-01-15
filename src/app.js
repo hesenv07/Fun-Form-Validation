042bdc6b64ef890c5f0b2df1e14a2859ebf37f0e
hesenv07 <arifhesenv@gmail.com>
2023-01-15 23:11:52 +0400

    Change button position

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const btn = document.querySelector(".btn");
+const formEl = document.querySelector(".form");
+
+var position;
+
+btn.addEventListener("mouseover", function () {
+  if (!formEl.checkValidity()) {
+    position ? (position = 0) : (position = 100);
+    btn.style.transform = `translate(${position}px, 0px)`;
+    btn.style.transition = "all 0.3s ease";
+  } else {
+    return;
+  }
+});
+
+btn.addEventListener("click", function () {
+  e.preventDefault();
+  alert("welldone");
+});
